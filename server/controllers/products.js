776b971bd4525035353a10968e0ccfac8c9f1d4d
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-03-18 16:52:24 -0300

    Added some functionalities.
    Added some functions to catch some errors, and a mock array of products to test an endpoint.

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,6 +1,27 @@
-const getAll = (req, res) => {
+const products = [
+    {
+        id: '1',
+        title: 'Apuntes Javascript',
+        price: 200,
+        description: 'Los mejores apuntes de Javascript'
+    },
+    {
+        id: '2',
+        title: 'Apuntes Python',
+        price: 170,
+        description: 'Los mejores apuntes de Python'
+    },
+    {
+        id: '3',
+        title: 'Apuntes PHP',
+        price: 150,
+        description: 'Los mejores apuntes de PHP'
+    }
+]
+
+const all = (req, res) => {
     res.json(
-        {name: "bautista", nickname: "iceman"}
+        {products}
     );
 }
 
@@ -9,4 +30,4 @@ const create = (req, res) => {
     res.send();
 }
 
-module.exports = { getAll, create } 
+module.exports = { all, create } 
--- server/index.js
+++ server/index.js
@@ -15,4 +15,16 @@ app.use(bodyParser.json());
 
 app.use('/products', products)
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    res.sendStatus(404); // 404
+});
+  
+// error handler
+app.use(function (err, _, res, __) {
+    console.error(err); // log forever
+    // console.info | warn | log | error
+    res.sendStatus(500);
+});
+
 app.listen(port, () => console.log(`Example app listening on portttt ${port}!`))
