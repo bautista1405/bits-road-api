c8a525a8130151d939d9d6f91153a77b1845c3d3
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-03-24 20:57:41 -0300

    Retrieve all purchases per user.
    Created a functionality to retrieve a user all of its purchases.

--- server/controllers/purchases.js
+++ server/controllers/purchases.js
@@ -1,5 +1,13 @@
-const all = (req, res) => {
-    res.json({ message: 'todas tus compras'})
-}
+const Purchase = require("../models/purchase");
+
+const all = async (req, res) => {
+    try {
+      const data = await Purchase.find({ users: req.id });
+      res.json({ message: `Todas las compras del usuario ${req.id}`, data });
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(500);
+    }
+};
 
 module.exports = { all }
