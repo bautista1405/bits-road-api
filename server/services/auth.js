dc25a6c6c120695988f255c6486ff30f5344eedb
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2022-04-11 21:55:48 -0300

    Updated auth.

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs'); // file system
-const privateKey = fs.readFileSync('server/keys/private.pem');
-const publicKey = fs.readFileSync('server/keys/public.pem')
+// const privateKey = fs.readFileSync('server/keys/private.pem');
+// const publicKey = fs.readFileSync('server/keys/public.pem')
 const signOptions = { expiresIn: '8h', algorithm: 'RS256' };
 
 const createToken = (payload) => jwt.sign(payload, privateKey, signOptions); //creamos el token
